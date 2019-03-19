1ed647006b76b47c53bd8e1ec04112b2398a7eca
Judith Verstegen <judithverstegen@gmail.com>
2019-03-19 11:34:38 +0100

    corrected bug in the postloop script that copies the 2030 maps to the results folder.

--- copy2030maps.py
+++ copy2030maps.py
@@ -1,28 +1,19 @@
 import os
 import numpy as np
 import Parameters
+import ParametersProjection
 import sys
 import shutil
 from pcraster import *
 
-nrOfSamples=Parameters.getNrSamples()
-nrOfTimesteps=Parameters.getNrTimesteps()
-
+nrOfSamples= ParametersProjection.getNrSamplesFromFile('particle_mapping.csv')    
 sampleNumbers=range(1,nrOfSamples+1,1)
-timeSteps=range(1,nrOfTimesteps+1,1)
-print timeSteps
 
 for aSample in sampleNumbers:
   src = os.path.join(str(aSample), 'landUse0.025')
   dst = os.path.join('results', '2030_' + str(aSample) + '.map')
   shutil.copy2(src, dst)
-  # also for 2015, 2020, and 2025 for the optimization
-  src = os.path.join(str(aSample), 'landUse0.020')
-  dst = os.path.join('results', '2025_' + str(aSample) + '.map')
-  shutil.copy2(src, dst)
+  # also 2020, can be removed if required
   src = os.path.join(str(aSample), 'landUse0.015')
   dst = os.path.join('results', '2020_' + str(aSample) + '.map')
-  shutil.copy2(src, dst)
-  src = os.path.join(str(aSample), 'landUse0.010')
-  dst = os.path.join('results', '2015_' + str(aSample) + '.map')
   shutil.copy2(src, dst)
